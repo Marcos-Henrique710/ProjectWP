573cfe7aaec33aaa5f6b52d72e0b6a8813cd2c32
Marcos_Henrique_710 <marcoshenriqueufrn@gmail.com>
2026-03-05 10:57:53 -0300

    inicio do projeto da unidade curicular de programação web

--- src/main/java/com/example/projectwp/ProjectWpApplication.java
+++ src/main/java/com/example/projectwp/ProjectWpApplication.java
@@ -2,6 +2,7 @@ package com.example.projectwp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class ProjectWpApplication {
@@ -9,5 +10,5 @@ public class ProjectWpApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectWpApplication.class, args);
     }
-
+    G
 }
