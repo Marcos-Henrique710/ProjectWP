883dbb075ead7fc73bf59a9c96493939218c16df
Marcos_Henrique_710 <marcoshenriqueufrn@gmail.com>
2026-03-05 10:54:00 -0300

    inicio do projeto da unidade curicular de programação web

---
+++ src/test/java/com/example/projectwp/ProjectWpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectwp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
